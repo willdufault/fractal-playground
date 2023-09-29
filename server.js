7fe1f1517bdf56a629675a4d3972eac82ee803a5
wduf <wduf02@gmail.com>
2023-09-29 14:42:12 -0400

    static -> public

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let port = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+app.use(express.static('public'));
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}!`)
