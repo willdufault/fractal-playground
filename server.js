63c3ef79332be9e596013f3bf51b409c1ffbfd64
wduf <wduf02@gmail.com>
2025-07-19 11:49:54 -0400

    removed node server, static page now

--- server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-let port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
-app.listen(port, () => {
-	console.log(`App listening on port ${port}!`)
-})
